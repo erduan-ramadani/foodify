290e8ee54db293cbc21ff623ecea512868782680
Erduan Ramadani <ramadani.erduan@web.de>
2026-04-23 14:44:22 +0200

    fix: center date selector in top bar with spacer counterweight

--- composeApp/src/androidMain/kotlin/com/ercoding/foodify/presentation/dashboard/components/FoodifyTopAppBar.kt
+++ composeApp/src/androidMain/kotlin/com/ercoding/foodify/presentation/dashboard/components/FoodifyTopAppBar.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -32,6 +33,9 @@ fun FoodifyTopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.background
         ),
+        navigationIcon = {
+            Spacer(modifier = Modifier.size(48.dp))
+        },
         title = {
             DateSelector(viewModel)
         },
