43da726bb7ff2c0c8e784b9fc0ec172cf46b7e2d
Erduan Ramadani <ramadani.erduan@web.de>
2026-04-28 16:12:11 +0200

    chore: add missing methods to fake repository

--- composeApp/src/test/kotlin/com/ercoding/foodify/data/local/FakePreferencesRepository.kt
+++ composeApp/src/test/kotlin/com/ercoding/foodify/data/local/FakePreferencesRepository.kt
@@ -1,17 +1,28 @@
 package com.ercoding.foodify.data.local
 
 import com.ercoding.foodify.domain.PreferencesInterface
+import com.ercoding.foodify.domain.model.onboarding.OnboardingData
 import com.ercoding.foodify.domain.model.sheet.NutritionEntry
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
-class FakePreferencesRepository() : PreferencesInterface {
+class FakePreferencesRepository(
+    override val reminder: Flow<Boolean>,
+    override val onboardingData: Flow<OnboardingData?>
+) : PreferencesInterface {
     override val darkMode: Flow<Boolean>
         get() = flowOf(false)
     override val nutritionEntries: Flow<String?>
         get() = flowOf(null)
 
     override suspend fun setDarkMode(enabled: Boolean) {}
+    override suspend fun setReminder(enabled: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun setOnboardingData(onboardingData: OnboardingData?) {
+        TODO("Not yet implemented")
+    }
 
     override suspend fun setNutritionEntries(entries: List<NutritionEntry>) {}
 
