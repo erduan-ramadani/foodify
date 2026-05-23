da6326b808477bf722ffeabd11fa0e95aea94205
Erduan Ramadani <ramadani.erduan@web.de>
2026-05-23 10:18:42 +0200

    chore: bump version code to 4

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -95,7 +95,7 @@ android {
         applicationId = "com.eddiapps.foodify"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
     }
     packaging {
