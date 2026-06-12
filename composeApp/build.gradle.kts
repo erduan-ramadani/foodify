1921c27394dc06429518006a15471681aa6530fd
Erduan Ramadani <ramadani.erduan@web.de>
2026-06-12 16:50:39 +0200

    chore: bump version code to 11

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -97,7 +97,7 @@ android {
         applicationId = "com.eddiapps.foodify"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 10
+        versionCode = 11
         versionName = "1.0.1"
     }
     packaging {
