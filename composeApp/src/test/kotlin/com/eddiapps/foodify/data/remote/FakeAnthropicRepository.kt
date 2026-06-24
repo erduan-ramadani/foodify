0d4cb0735b9d5285a13d62442247c1a1005fc8d0
Erduan Ramadani <ramadani.erduan@web.de>
2026-06-24 14:17:51 +0200

    feat: add fake repositories for testing

--- composeApp/src/test/kotlin/com/eddiapps/foodify/data/remote/FakeAnthropicRepository.kt
+++ composeApp/src/test/kotlin/com/eddiapps/foodify/data/remote/FakeAnthropicRepository.kt
@@ -3,8 +3,27 @@ package com.eddiapps.foodify.data.remote
 import com.eddiapps.foodify.domain.AnthropicInterface
 import com.eddiapps.foodify.domain.model.sheet.NutritionEntry
 
-class FakeAnthropicRepository(private val returnValue: Int = 35) : AnthropicInterface {
-    override suspend fun requestNutritionValues(query: String): Result<NutritionEntry> {
-        return Result.success(NutritionEntry())
+class FakeAnthropicRepository(
+    private var nextResult: Result<NutritionEntry>
+    = Result.success(NutritionEntry(title = "Default"))
+) : AnthropicInterface {
+
+    var lastQuery: String? = null
+        private set
+
+    fun setNextResult(result: Result<NutritionEntry>) {
+        nextResult = result
+    }
+
+    override suspend fun requestNutritionValues(
+        query: String,
+        weight: Double
+    ): Result<NutritionEntry> {
+        lastQuery = query
+        return nextResult
+    }
+
+    override suspend fun requestNutritionValuesFromImage(base64Image: String): Result<NutritionEntry> {
+        return nextResult
     }
 }
